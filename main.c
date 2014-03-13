59946c4edf76bb3778957380065fd916a06b7aa4
JackHsu <jackhsu@jackhsu-Aspire-4810T.(none)>
2014-03-13 22:45:03 +0800

    Delay

--- main.c
+++ main.c
@@ -95,7 +95,7 @@ void delay_gg(uint32_t count){
 int main(void)
 {
 	int16_t buff[6];
-	float acc[3],gyro[3],num=3.4444;
+	float acc[3],gyro[3];
 	float theta;
 	float phi;
 	float error;
@@ -115,7 +115,7 @@ int main(void)
 	}else {
 	   puts("connection failed\r\n");
 	}
-	printf("test float%f\r\n",num);
+	//printf("test float%f\r\n",num);
 	while (1) {
 		//puts("running now\r\n");
 		MPU6050_GetRawAccelGyro(buff);
@@ -124,7 +124,7 @@ int main(void)
 		for ( int i = 0; i<3; i++)
 			gyro[i] = (buff[i+2]/131.0);
 
-		theta = atanf(acc[2]/acc[0]);
+		theta = atanf(acc[0]/acc[2);
 		error = setpoint - theta;
 		derivative = gyro[1];
 
@@ -158,6 +158,7 @@ int main(void)
 
     gpio_toggle(GPIOA, GPIO_Pin_0);
     gpio_toggle(GPIOA, GPIO_Pin_1);
+    delay_gg(1);
 
 	}
 }
