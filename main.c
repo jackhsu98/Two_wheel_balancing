d8da30aad7476f0872f41b52c6494d71571ddb59
JackHsu <jackhsu@jackhsu-Aspire-4810T.(none)>
2014-03-13 22:50:45 +0800

    TIM2

--- main.c
+++ main.c
@@ -70,6 +70,39 @@ void init_tim4_pwm()
 	  /* TIM3 enable counter */
 	  TIM_Cmd(TIM4, ENABLE);
 }
+
+void init_tim2()
+{	
+	//RCC_PCLK1Config(RCC_HCLK_Div2);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+
+	NVIC_InitTypeDef NVIC_InitStructure;
+
+	/* Enable the TIM2 global Interrupt */
+	NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+
+	NVIC_Init(&NVIC_InitStructure);
+
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+	/* Time base configuration */
+	TIM_TimeBaseStructure.TIM_Period = (uint16_t)(7200-1);
+	TIM_TimeBaseStructure.TIM_Prescaler = (uint16_t)(10000-1);
+
+	TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+
+	TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+
+	// /* Prescaler configuration */
+	// TIM_PrescalerConfig(TIM2, PrescalerValue, TIM_PSCReloadMode_Immediate);
+
+	TIM_ITConfig( TIM2, TIM_IT_Update, ENABLE);
+	TIM_Cmd(TIM2, ENABLE);
+}
+
 void init_led()
 {
 	GPIO_InitTypeDef GPIO_InitStructure;
@@ -82,10 +115,12 @@ void init_led()
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 }
+
 void gpio_toggle(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin)
 {
 	GPIOx->ODR ^= GPIO_Pin;
 }
+
 void delay_gg(uint32_t count){
     timee=0;
 	while(count>timee){}
